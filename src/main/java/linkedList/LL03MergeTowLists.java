d31b92f52e91dc505fbb71553b7c5b81a3edc0df
daili <daili@cmss.chinamobile.com>
2022-02-12 22:58:07 +0800

    新增链表样例
    Code Source From: Self Code
    Description:  [Optional]
    Jira:  #[Optional]
    市场项目编号（名称）：[Optional]

--- src/main/java/linkedList/LL03MergeTowLists.java
+++ src/main/java/linkedList/LL03MergeTowLists.java
@@ -15,6 +15,7 @@ public class LL03MergeTowLists {
     }
 
     /**
+     * 方法一：递归
      * 思路
      * 标签：链表、递归
      * 这道题可以使用递归实现，新链表也不需要构造新节点，我们下面列举递归三个要素
@@ -22,12 +23,18 @@ public class LL03MergeTowLists {
      * 返回值：每一层调用都返回排序好的链表头
      * 本级递归内容：如果 l1 的 val 值更小，则将 l1.next 与排序好的链表头相接，l2 同理
      * O(m+n) m 为 l1的长度，n为 l2 的长度
+     *
+     * 时间复杂度：O(n + m)，其中 n 和 m 分别为两个链表的长度。因为每次调用递归都会去掉 l1 或者 l2 的头节点（直到至少有一个
+     * 链表为空），函数 mergeTwoList 至多只会递归调用每个节点一次。因此，时间复杂度取决于合并后的链表长度，即 O(n+m)。
+     *
+     * 空间复杂度：O(n + m)，其中 n 和 m 分别为两个链表的长度。递归调用 mergeTwoLists 函数时需要消耗栈空间，栈空间的大小
+     * 取决于递归调用的深度。结束递归调用时 mergeTwoLists 函数最多调用 n+m 次，因此空间复杂度为O(n+m)。
      * @param l1
      * @param l2
      * @return
      */
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+/*    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) {
             return l2;
         }
@@ -42,5 +49,57 @@ public class LL03MergeTowLists {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
+    }*/
+
+    /**
+     * 方法二：迭代
+     * 思路
+     *
+     * 我们可以用迭代的方法来实现上述算法。当 l1 和 l2 都不是空链表时，判断 l1 和 l2 哪一个链表的头节点的值更小，将较小值的
+     * 节点添加到结果里，当一个节点被添加到结果里之后，将对应链表中的节点向后移一位。
+     *
+     * 算法
+     *
+     * 首先，我们设定一个哨兵节点 prehead ，这可以在最后让我们比较容易地返回合并后的链表。我们维护一个 prev 指针，我们需要
+     * 做的是调整它的 next 指针。然后，我们重复以下过程，直到 l1 或者 l2 指向了 null ：如果 l1 当前节点的值小于等于 l2 ，
+     * 我们就把 l1 当前的节点接在 prev 节点的后面同时将 l1 指针往后移一位。否则，我们对 l2 做同样的操作。不管我们将哪一个
+     * 元素接在了后面，我们都需要把 prev 向后移一位。
+     *
+     * 在循环终止的时候， l1 和 l2 至多有一个是非空的。由于输入的两个链表都是有序的，所以不管哪个链表是非空的，它包含的所有
+     * 元素都比前面已经合并链表中的所有元素都要大。这意味着我们只需要简单地将非空链表接在合并链表的后面，并返回合并链表即可。
+     *
+     * 下图展示了 1->4->5 和 1->2->3->6 两个链表迭代合并的过程：
+     *
+     * 复杂度分析
+     *
+     * 时间复杂度：O(n+m)，其中 n 和 m 分别为两个链表的长度。因为每次循环迭代中，l1 和 l2 只有一个元素会被放进合并链表中，
+     * 因此 while 循环的次数不会超过两个链表的长度之和。所有其他操作的时间复杂度都是常数级别的，
+     * 因此总的时间复杂度为 O(n+m)。
+     *
+     * 空间复杂度：O(1)。我们只需要常数的空间存放若干变量。
+     */
+
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(0);
+
+        ListNode prev = dummyNode;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+
+            prev = prev.next;
+        }
+
+        prev.next = l1 == null ?l2:l1;
+
+        return dummyNode.next;
     }
+
 }
